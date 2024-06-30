aedd5ee29994dfed7fe5bac83541c6793ceabcce
Takayuki Yamauchi <tyamauch@c3r5s6.42tokyo.jp>
2024-06-30 20:03:13 +0900

    format

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -2,17 +2,17 @@
 #define FIXED_HPP
 
 class Fixed {
-       private:
-	int num;
-	static const int cnum;
+ private:
+  int num;
+  static const int cnum;
 
-       public:
-	Fixed();
-	~Fixed();
-	Fixed(const Fixed &f);
-	Fixed &operator=(const Fixed &f);
-	int getRawBits(void) const;
-	void setRawBits(int const raw);
+ public:
+  Fixed();
+  ~Fixed();
+  Fixed(const Fixed &f);
+  Fixed &operator=(const Fixed &f);
+  int getRawBits(void) const;
+  void setRawBits(int const raw);
 };
 
 #endif
