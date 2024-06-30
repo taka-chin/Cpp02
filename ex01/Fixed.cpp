1bbeab567278f0d7732a801879947a28d3837222
Takayuki Yamauchi <tyamauch@c3r5s6.42tokyo.jp>
2024-06-30 22:09:00 +0900

    totyu

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,65 @@
+#include "Fixed.hpp"
+#include <cmath>
+
+#include <iostream>
+
+Fixed::Fixed() : fixedPointNum(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int inputNum) {
+	std::cout << "int constructor called" << std::endl;
+	fixedPointNum = inputNum << digits;
+}
+
+Fixed::Fixed(const float inputNum) {
+	std::cout << "float constructor called" << std::endl;
+	fixedPointNum = roundf(inputNum * (1 << digits));
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &f) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = f;
+}
+
+Fixed &Fixed::operator=(const Fixed &f) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->fixedPointNum = f.getRawBits();
+	return *this;
+}
+
+
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->fixedPointNum;
+}
+
+void Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->fixedPointNum = raw;
+}
+
+float toFloat( void ) const
+{
+	int fixedPointNum = Fixed::getRawBits();
+	float ret = float(fixedPointNum / (1 << 8));
+	return ret; 
+}
+
+int toInt( void ) const
+{
+	int fixedPointNum = Fixed::getRawBits();
+	int ret = fixedPointNum >> digits; 
+	return ret;	
+}
+
+std::ostream &operator<<(std::ostream &output)
+{
+		output << Fixed::toFloat(); 
+		return output;
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,25 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <string>
+
+class Fixed {
+       private:
+  int fixedPointNum;
+  static const int digits = 8;
+
+       public:
+	Fixed();
+	Fixed(const int inputNum);
+	Fixed(const float inputNum);
+	~Fixed();
+	Fixed(const Fixed &f);
+	Fixed &operator=(const Fixed &f);
+	std::ostream &operator<<(std::ostream &output);
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat( void ) const;
+	int toInt( void ) const;
+};
+
+#endif
