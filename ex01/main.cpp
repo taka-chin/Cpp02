83a9dd3f31652aea9c7ce9ffcd106122368521ed
taka-chin <ty80164@gmail.com>
2024-07-02 12:27:21 +0900

    test

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@ Fixed a;
 Fixed const b( 10 );
 Fixed const c( 42.42f );
 Fixed const d( b );
-a = Fixed( 1234.4321f );
+	a = Fixed( 1234.4321f );
 
 std::cout << "a is " << a << std::endl;
 std::cout << "b is " << b << std::endl;
