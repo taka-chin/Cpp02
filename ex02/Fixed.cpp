83fbb576efd3f0dc40a561bd143d7a423dd0f235
Takayuki Yamauchi <tyamauch@c3r1s1.42tokyo.jp>
2024-07-03 16:03:03 +0900

    ss

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,94 @@
+#include "Fixed.hpp"
+#include <cmath>
+
+#include <iostream>
+
+Fixed::Fixed() : fixedPointNum(0) {
+  std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int inputNum) {
+  std::cout << "Int constructor called" << std::endl;
+  fixedPointNum = inputNum << digits;
+}
+
+Fixed::Fixed(const float inputNum) {
+  std::cout << "Float constructor called" << std::endl;
+  fixedPointNum = roundf(inputNum * (1 << digits));
+}
+
+Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
+
+Fixed::Fixed(const Fixed &f) {
+  std::cout << "Copy constructor called" << std::endl;
+  *this = f;
+}
+
+Fixed &Fixed::operator=(const Fixed &f) {
+  std::cout << "Copy assignment operator called" << std::endl;
+  this->fixedPointNum = f.getRawBits();
+  return *this;
+}
+
+int Fixed::getRawBits(void) const {
+  /* std::cout << "getRawBits member function called" << std::endl; */
+  return this->fixedPointNum;
+}
+
+void Fixed::setRawBits(int const raw) {
+  std::cout << "setRawBits member function called" << std::endl;
+  this->fixedPointNum = raw;
+}
+
+float Fixed::toFloat(void) const {
+  return static_cast<float>(fixedPointNum )/ (1 << digits);
+}
+
+int Fixed::toInt(void) const {
+  return fixedPointNum >> digits;
+}
+
+std::ostream &operator<<(std::ostream &output,const Fixed &f) {
+  output << f.toFloat();
+  return output;
+}
+
+Fixed operator+(const Fixed &f){
+  std::cout << "plus assignment operator called" << std::endl;
+}
+
+Fixed operator-(const Fixed &f){
+  std::cout << "minus assignment operator called" << std::endl;
+}
+
+Fixed operator*(const Fixed &f){
+  std::cout << "plus assignment operator called" << std::endl;
+}
+
+Fixed operator/(const Fixed &f){
+  std::cout << "plus assignment operator called" << std::endl;
+}
+
+bool operator>(const Fixed &f){
+  std::cout << "plus assignment operator called" << std::endl;
+}
+
+bool operator<(const Fixed &f){
+  std::cout << "plus assignment operator called" << std::endl;
+}
+
+bool operator>=(const Fixed &f){
+  std::cout << "plus assignment operator called" << std::endl;
+}
+
+bool operator<=(const Fixed &f){
+  std::cout << "plus assignment operator called" << std::endl;
+}
+
+bool operator==(const Fixed &f){
+  std::cout << "plus assignment operator called" << std::endl;
+}
+
+bool operator!=(const Fixed &f){
+  std::cout << " assignment operator called" << std::endl;
+}
---
+++ ex02/Fixed.hpp
@@ -0,0 +1,39 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <string>
+
+class Fixed {
+private:
+  int fixedPointNum;
+  static const int digits = 8;
+
+public:
+  Fixed();
+  Fixed(const int inputNum);
+  Fixed(const float inputNum);
+  ~Fixed();
+  Fixed(const Fixed &f);
+  Fixed &operator=(const Fixed &f);
+  bool operator>(const Fixed &f);
+  bool operator<(const Fixed &f);
+  bool operator>=(const Fixed &f);
+  bool operator<=(const Fixed &f);
+  bool operator==(const Fixed &f);
+  bool operator!=(const Fixed &f);
+  Fixed operator+(const Fixed &f);
+  Fixed operator-(const Fixed &f);
+  Fixed operator*(const Fixed &f);
+  Fixed operator/(const Fixed &f);
+  static Fixed &min(Fixed &f1,Fixed &f2);
+  static const Fixed &min(const Fixed &f1,const Fixed &f2);
+  static Fixed &max(Fixed &f1,Fixed &f2);
+  static const Fixed &max(const Fixed &f1,const Fixed &f2);
+  int getRawBits(void) const;
+  void setRawBits(int const raw);
+  float toFloat(void) const;
+  int toInt(void) const;
+};
+
+std::ostream &operator<<(std::ostream &output,const Fixed &f);
+#endif
